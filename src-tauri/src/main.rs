1c9105c847f9b1a2bdc6d3ae966bcb00975681e8
HooEP01 <hooernping02@gmail.com>
2023-07-01 18:27:00 +0800

    tauri window menu

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,68 +1,97 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-use std::{thread, time};
 use tauri::Manager;
-use tauri::{App, CustomMenuItem, SystemTray, SystemTrayEvent, SystemTrayMenu};
+
+// Window Menu
+use tauri::{CustomMenuItem, Menu, MenuItem, Submenu};
+
+// Window
+use tauri::WindowBuilder;
+
+// System Tray
+use tauri::{SystemTray, SystemTrayMenu};
 
 #[derive(Clone, serde::Serialize)]
 struct Payload {
     message: String,
 }
 
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
-
 fn main() {
-    use {
-        std::thread::{sleep, spawn},
-        std::time::Duration,
-    };
+    // Menu Instance
+    let quit = CustomMenuItem::new("quit".to_string(), "Quit");
+    let close = CustomMenuItem::new("close".to_string(), "Close");
+    let submenu = Submenu::new("File", Menu::new().add_item(quit).add_item(close));
+    let menu = Menu::new()
+        .add_native_item(MenuItem::Copy)
+        .add_item(CustomMenuItem::new("hide", "Hide"))
+        .add_submenu(submenu);
 
-    // let msg = String::from("Hello, world!");
-    // println!("Message from Rust: {}", msg);
+    let window_menu = Menu::new().add_item(CustomMenuItem::new("quit".to_string(), "King"));
+
     let tray_menu = SystemTrayMenu::new();
     let system_tray = SystemTray::new().with_menu(tray_menu);
 
     tauri::Builder::default()
         .setup(|app| {
+            let window = WindowBuilder::new(
+                app,
+                "main-window".to_string(),
+                tauri::WindowUrl::App("index.html".into()),
+            )
+            .menu(window_menu)
+            .build()?;
+
+            let window_ = window.clone();
+            window.on_menu_event(move |event| match event.menu_item_id() {
+                "quit" => {
+                    std::process::exit(0);
+                }
+                "close" => {
+                    window_.close().unwrap();
+                }
+                _ => {}
+            });
+
+            let main_window = app.get_window("main").unwrap();
+            let menu_handle = main_window.menu_handle();
+            std::thread::spawn(move || {
+                // you can also `set_selected`, `set_enabled` and `set_native_image` (macOS only).
+                menu_handle.get_item("item_id").set_title("New title");
+            });
+
             let app_ = app.handle();
 
             // listen to the `event-name` (emitted on any window)
             app.listen_global("send-message", move |event| {
                 println!("got event-name with payload {:?}", event.payload());
                 app_.emit_all(
-                  "message-back-end",
-                  Payload {
-                      message: "Tauri is awesome! (coming from back-end)".into(),
-                  },
-              )
-              .unwrap();
+                    "message-back-end",
+                    Payload {
+                        message: "Tauri is awesome! (coming from back-end)".into(),
+                    },
+                )
+                .unwrap();
             });
 
-
             // 3 --- emit event to frontend
-            
 
             // app.unlisten(id);
 
             Ok(())
         })
+        .menu(menu)
+        .on_menu_event(|event| match event.menu_item_id() {
+            "quit" => {
+                std::process::exit(0);
+            }
+            "close" => {
+                event.window().close().unwrap();
+            }
+            _ => {}
+        })
         .system_tray(system_tray)
-        .invoke_handler(tauri::generate_handler![greet, background_function_1,])
+        .invoke_handler(tauri::generate_handler![])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
-}
-
-#[tauri::command]
-fn background_function_1() {
-    println!("Hello from background function 1");
-
-    let ten_millis = time::Duration::from_millis(10);
-    // let now = time::Instant::now();
-    thread::sleep(ten_millis);
-    println!("Hello from background function 1");
 }
